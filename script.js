fb883afdba6531baca98b4b56feb787ff9a39e5a
Afiq <qifacomics@gmail.com>
2023-02-19 18:57:49 +0800

    Created webpage that resembles a calculator

---
+++ script.js
@@ -0,0 +1,24 @@
+function addNum(num1, num2) {
+    result = num1 + num2;
+    return result;
+}
+
+function subtractNum(num1, num2) {
+    result = num1 - num2;
+    return result;
+}
+
+function multiplyNum(num1, num2) {
+    result = num1 * num2;
+    return result;
+}
+
+function divideNum(num1, num2) {
+    result = num1 / num2;
+    return result;
+}
+
+console.log(addNum(2,2));
+console.log(subtractNum(4,2));
+console.log(multiplyNum(2,2));
+console.log(divideNum(4,2));
