f9a45972df5d11049792f584289002a189ffb59d
Afiq <qifacomics@gmail.com>
2023-02-20 01:16:00 +0800

    Redid styling to represent iOS calculator app

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-function addNum(num1, num2) {
-    result = num1 + num2;
-    return result;
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalButton = document.querySelector('.equals');
+const decimalButton = document.querySelector('.decimal');
+const previousDisplay = document.querySelector('.previous');
+const currentDisplay = document.querySelector('.current');
+
+let previousNum = '';
+let currentNum = '';
+let operator = '';
+
+numberButtons.forEach(number => {
+    number.addEventListener('click', (e) => {
+        handleNum(e.target.textContent);
+    })
+})
+
+operatorButtons.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        handleOperator(e.target.textContent);
+    })
+})
+
+function handleNum(num) {
+    if (currentNum.length < 9) {
+        currentNum += num;
+        currentDisplay.textContent = currentNum;
+    }
+    else {
+        // this prevent users from inputting more than 9 digits
+    }
 }
 
-function subtractNum(num1, num2) {
-    result = num1 - num2;
-    return result;
+function handleOperator(op) {
+    operator = op;
+    previousNum = currentNum;
+    console.log(previousNum);
+    previousDisplay.textContent = previousNum + " " + operator + " ";
+    currentNum = '';
 }
-
-function multiplyNum(num1, num2) {
-    result = num1 * num2;
-    return result;
+function operate() {
+    let previousNumber = Number(previousNum);
+    let currentNumber = Number(currentNum);
+    // to be continued
 }
-
-function divideNum(num1, num2) {
-    result = num1 / num2;
-    return result;
-}
-
-console.log(addNum(2,2));
-console.log(subtractNum(4,2));
-console.log(multiplyNum(2,2));
-console.log(divideNum(4,2));
